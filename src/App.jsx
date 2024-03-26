0c12e2a6a9c55bf9413743b087712fdccfe4aef0
juanpahv <dcrunneryt1@gmail.com>
2024-03-26 17:19:19 -0600

    sort movies functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,33 @@ import Movies from "./Movies"
 import useSearch from "./hooks/useSearch"
 import useMovies from "./hooks/useMovies"
 import './App.css'
+import { useState } from "react"
 
 function App() {
   const { search, error,  setSearch} = useSearch()
-  const { movies, getMovies } = useMovies({ search })
+  const [sort, setSort] = useState(false)
+  const { movies, getMovies } = useMovies({ search, sort })
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    getMovies()
+    getMovies({ search })
   }
 
   const handleChange = (event) => {
     const newSearch = event.target.value
-    setSearch(newSearch)
+    setSearch({ search: newSearch })
   }
 
+  const handleSort = () => {
+    setSort(!sort)
+  }
   return (
     <>
     <h1>Search movies</h1>
     <form onSubmit={handleSubmit}>
         <input name="search" type="text" placeholder="Harry Potter, Avengers, Spiderman" onChange={handleChange} value={search}/>
         <button type="submit">Search</button>
+        <input type="checkbox" name="sort" onChange={handleSort} checked={sort} />
     </form>
     {
       error && <strong style={{color: 'red'}}>{error}</strong>
--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,21 +1,27 @@
 import results from '../mocks/response.json'
 import noResults from '../mocks/no-results.json'
-import { useRef, useState } from 'react'
+import { useRef, useState, useCallback } from 'react'
 import { searchMovies } from '../services/movies'
 
-export default function useMovies({ search }) {
+export default function useMovies({ search, sort }) {
   const [movies, setMovies] = useState([])
   const previousSearch = useRef(search)
 
-  const getMovies = async () => {
+  const getMovies = useCallback(async ({ search }) => {
     // evitar hacer la misma búsqueda
     if (search === previousSearch.current) return
     // actualizar la búsqueda anterior
     previousSearch.current = search
 
     const newMovies = await searchMovies({ search })
-    setMovies(newMovies)
-  }
 
-  return { movies, getMovies }
+    setMovies(newMovies)
+  }, [])
+
+  const getSortedMovies = useCallback(() => {
+    const sortedMovies = sort ? [...movies].sort((a, b) => a.title.localeCompare(b.title)) : movies
+    return sortedMovies
+  }, [sort, movies])
+
+  return { movies: getSortedMovies(movies), getMovies }
 }
